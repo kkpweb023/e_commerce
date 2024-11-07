a2b5d086a9345d9c56be725739acce4de9b5878c
kkpweb023 <kpandey023@gmail.com>
2024-11-07 22:29:45 +0530

    first commit 3

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/e-commerce">
+    <BrowserRouter basename="/e_commerce">
       <App />
     </BrowserRouter>
   </React.StrictMode>
